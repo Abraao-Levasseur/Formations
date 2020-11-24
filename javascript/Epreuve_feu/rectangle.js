9c8ff9310832e4cb0f3ff0bd073b47d5e3c1c441
Abraao-Levasseur <abraao.levasseur@gmail.com>
2020-11-24 17:45:21 +0100

    javascript

--- javascript/Epreuve_feu/rectangle.js
+++ javascript/Epreuve_feu/rectangle.js
@@ -1,24 +1,31 @@
+//Créer un script qui affiche la position du carré passé en premier argument dans le carré du second argument
 var fs = require('fs');
 let tableau1 =  fs.readFileSync('./' + process.argv[2], 'utf8').split(['\n']);
 let tableau2 =  fs.readFileSync('./' + process.argv[3], 'utf8').split(['\n']);
 tableau1_ = [];
 tableau2_ = [];
+// for (let i = 0; i < tableau1.length; i++){
+//     tableau1__ = tableau1_.push(tableau1[i].split(''));
+// }
+// for (let i = 0; i < tableau2.length; i++){
+//     tableau2__ = tableau2_.push(tableau2[i].split(''));
+// }
+
 for (let i = 0; i < tableau1.length; i++){
     tableau1__ = tableau1_.push(tableau1[i].split(''));
-}
-for (let i = 0; i < tableau2.length; i++){
+    for (let i = 0; i < tableau2.length; i++){
     tableau2__ = tableau2_.push(tableau2[i].split(''));
 }
-
+}
 function correspondance2(tableau1_, tableau2_, ligne, colonne){
     for( let ligne_ = 0; ligne_ < tableau1_.length; ligne_++){
         for( let colonne_ = 0; colonne_ < tableau1_[ligne_].length; colonne_++){
             if(ligne + ligne_ < tableau2_.length && colonne + colonne_ < tableau2_[1].length 
             && tableau1_[ligne_][colonne_] === tableau2_[ligne + ligne_][colonne + colonne_]){
                 // ici on vérifie si on trouve bien le tableau 1 dans le tableau 2
-                console.log(tableau2_[ligne + ligne_][colonne + colonne_]);
-                // ici on trouve les positions dans le tableau 2 correspondant au tableau 1
-                //console.log(ligne + ligne_ + "|" + (colonne + colonne_ )+ "\n");
+                //console.log(tableau2_[ligne + ligne_][colonne + colonne_]);
+                // ici on trouve la position du carré passé en premier argument dans le carré du second argument
+                console.log(ligne + ligne_ + "|" + (colonne + colonne_ )+ "\n");
             }else{
                 return 0;
             }
